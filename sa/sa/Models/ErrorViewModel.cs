9ce93b6ea80eac6e0fb0e189720f5c7af62d9feb
Saketh <22146977@student.westernsydney.edu.au>
2025-06-04 19:24:01 +1000

    Initial commit

---
+++ sa/sa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace sa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
