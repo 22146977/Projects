725d20b98fe3f1deab29f6a887884c49b09b4d8f
Saketh <22146977@student.westernsydney.edu.au>
2025-06-04 19:31:08 +1000

    Adding libraries

---
+++ Q1.R
@@ -0,0 +1,7 @@
+## Task 1: Average Stars using memeber_since Date
+# Upload Libraries
+library(dplyr) # For Manipulating Data
+library(ggplot2) # Used For Plotting Data
+library(lubridate) # Used For The Date
+library(knitr)
+library(kableExtra)
