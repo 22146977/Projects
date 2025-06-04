1b35a11c751720314f3ceee9c96a339f60c4b14f
Saketh <22146977@student.westernsydney.edu.au>
2025-06-04 19:44:39 +1000

    Converting to date format and filtering missing values

--- Q1.R
+++ Q1.R
@@ -8,3 +8,9 @@ library(kableExtra)
 
 # Read Users Data Csv File
 users <- read.csv("~/Downloads/R Project/users.csv", stringsAsFactors = FALSE)
+
+# We are converting member_since data to a data format, and then filtering this data
+cleanUsers <- users %>%
+  mutate(member_since = as.Date(member_since)) %>%
+  # Filtering out rows with missing values
+  filter(!is.na(member_since), !is.na(review_count), !is.na(average_stars)) %>%
